dbb315583c8dd5625da2456946d3f9d949eb1226
pedro.costa <pedro.costa@feedzai.com>
2020-06-02 21:52:23 +0100

    Add structure for counting the lines inside all files with extension xlsx

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # xlsxLineCounter
+
+This little program counts all lines in all files with xlsx extension in a folder. This program assumes that all data is on the first sheet and
+ that all sheets have a title line which is not considered as a line.
+ 
+ FOLDER_TO_CHECK_FOR_FILES -> folder to check for xlsx files (or another extension, but has to be an excel file)
+
+This project uses Quarkus, the Supersonic Subatomic Java Framework.
+
+## Running the application in dev mode
+
+You can run your application in dev mode that enables live coding using:
+```
+./mvnw quarkus:dev
+```
+
+## Packaging and running the application
+
+The application can be packaged using `./mvnw package`.
+It produces the `xlsx-line-counter-1.0.0-SNAPSHOT-runner.jar` file in the `/target` directory.
+Be aware that it’s not an _über-jar_ as the dependencies are copied into the `target/lib` directory.
+
+The application is now runnable using `java -jar target/xlsx-line-counter-1.0.0-SNAPSHOT-runner.jar`.
+
+## Creating a native executable
+
+You can create a native executable using: `./mvnw package -Pnative`.
+
+Or, if you don't have GraalVM installed, you can run the native executable build in a container using: `./mvnw package -Pnative -Dquarkus.native.container-build=true`.
+
+You can then execute your native executable with: `./target/xlsx-line-counter-1.0.0-SNAPSHOT-runner`
---
+++ src/main/java/com/pedro/costa/silva/ExampleResource.java
@@ -0,0 +1,51 @@
+package com.pedro.costa.silva;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+@Path("/hello")
+public class ExampleResource {
+
+    public static final String EXTENSION_FOR_FILES = ".xlsx";
+    public static final String FOLDER_TO_CHECK_FOR_FILES = "/home/pedro.costa/xlsFileCounter";
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() throws IOException {
+
+        int totalLinesInXlsxFile = 0;
+
+        List<String> fileNames = Files.walk(Paths.get(FOLDER_TO_CHECK_FOR_FILES))
+                                      .map(java.nio.file.Path::toString)
+                                      .filter(f -> f.endsWith(EXTENSION_FOR_FILES))
+                                      .collect(Collectors.toList());
+
+        for (String currentFile : fileNames) {
+            InputStream is = new FileInputStream(currentFile);
+            Workbook wb = WorkbookFactory.create(is);
+            Sheet sheet = wb.getSheetAt(0);
+            Iterator<Row> rowIter = sheet.rowIterator();
+
+            while (rowIter.hasNext()) {
+                totalLinesInXlsxFile++;
+                rowIter.next();
+            }
+        }
+
+        return "Counting files... Total lines:  " + (totalLinesInXlsxFile - fileNames.size()) + " in " + fileNames.size() +" files.";
+    }
+}
