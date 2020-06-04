2e926161bee052f62532ad07e57a01389f0fb0b1
pedro.costa <pedro.costa@feedzai.com>
2020-06-04 16:26:53 +0100

    Add boolean to indicate that each sheet has a header row

--- src/main/java/com/pedro/costa/silva/CountResource.java
+++ src/main/java/com/pedro/costa/silva/CountResource.java
@@ -1,21 +1,12 @@
 package com.pedro.costa.silva;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Collectors;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 /**
  * @author Pedro Costa (pedro.costa@feedzai.com).
@@ -24,9 +15,11 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 @Path("/count")
 public class CountResource {
 
+    public static final boolean XLS_HAS_HEADER_IN_EACH_SHEET = true;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String count() throws IOException {
+    public String count() {
 
         int totalLinesInXlsxFile = 0;
 
@@ -34,18 +27,22 @@ public class CountResource {
 
         for (String currentFile : fileNames) {
             Iterator<Row> rowIter = CommonUtils.rowIter(currentFile, 0);
-            if (rowIter!=null) {
+            if (rowIter != null) {
                 while (rowIter.hasNext()) {
                     totalLinesInXlsxFile++;
                     rowIter.next();
                 }
             }
         }
-        return "Counting files... Total lines:  " + (totalLinesInXlsxFile - fileNames.size()) + " in " + fileNames.size() +" files.";
+        return "Counting files... \n" + buildResponseMessage(totalLinesInXlsxFile, fileNames.size());
     }
 
-    private String buildResponseMessage(int totallines, boolean hasHeader, int numerOfFiles){
-return "";
-
+    private String buildResponseMessage(int totalLines, int numberOfFiles) {
+        return CountResource.XLS_HAS_HEADER_IN_EACH_SHEET ? "Total lines:  " + (totalLines - numberOfFiles) + " in " + numberOfFiles + " files." :
+                "Total lines:  "
+                        + totalLines
+                        + " in "
+                        + numberOfFiles
+                        + " files.";
     }
 }
